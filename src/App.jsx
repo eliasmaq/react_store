5ea768f0c1712e16b46303377f336d83e9922867
Camilo-J <josecamilo1902@outlook.com>
2023-03-15 17:52:17 -0500

    Add component footer in main Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
 import React from "react";
 import { useEffect, useState } from "react";
 import CardItem from "./components/CardItem";
 import styled from "styled-components";
+import Heading from "./components/Header";
+import Footer from "./components/Footer";
 
 const ContainerCard = styled("div")`
   display: flex;
 `;
-
-import Heading from "./components/Header";
-
 
 function App() {
   const [user, setUser] = useState(null);
@@ -19,25 +17,26 @@ function App() {
     return fetch("https://fakestoreapi.com/products")
       .then((response) => response.json())
       .then((data) => setUser(data));
-      
+  };
   useEffect(() => {
     fetchData();
-  }, [])
-  console.log(user)
+  }, []);
+  console.log(user);
 
   return (
-  <>
-   <Heading />
-    <ContainerCard>
-      {user?.map((item, index) => (
-        <CardItem
-          key={index}
-          itemName={item.title}
-          price={item.price}
-          photo={item.image}
-        ></CardItem>
-      ))}
-    </ContainerCard>
+    <>
+      <Heading />
+      <ContainerCard>
+        {user?.map((item, index) => (
+          <CardItem
+            key={index}
+            itemName={item.title}
+            price={item.price}
+            photo={item.image}
+          ></CardItem>
+        ))}
+      </ContainerCard>
+      <Footer />
     </>
   );
 }
